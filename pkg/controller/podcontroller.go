0ea2b1a3131932d5bce1ed8baddab8ac2b3d3a74
haitham911 <haitham911eg@gmail.com>
2021-07-16 13:52:34 +0200

    updates

--- pkg/controller/podcontroller.go
+++ pkg/controller/podcontroller.go
@@ -89,33 +89,64 @@ func (c *Controller) Run() {
 	informer := c.InformerFactory.Core().V1().Pods().Informer()
 
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    c.handleSchedAdd,
 		UpdateFunc: c.recreatePod,
+		DeleteFunc: c.handleSchedDelete,
 	})
+
 	informer.Run(c.CTX.Done())
 
 	<-c.CTX.Done()
 }
 
+/*
+func podLastTransitionTime(podObj *v1.Pod) time.Time {
+	for _, pc := range podObj.Status.Conditions {
+		if pc.Type == v1.PodScheduled && pc.Status == v1.ConditionFalse {
+			return pc.LastTransitionTime.Time
+		}
+	}
+	return time.Time{}
+}*/
 func (c *Controller) createInitialPods() {
 
 	for {
 
 		count := c.podCount("status.phase=Running", "manager=podcontroller")
 		count += c.podCount("status.phase=Pending", "manager=podcontroller")
-		c.Logger.Info("Running pods count ", zap.Int("count", count))
+		c.Logger.Info("Initia Running pods count ", zap.Int("count", count))
+		c.Logger.Info("Initia Pending pods count ", zap.Int("count", count))
 
 		for i := 0; i < c.RebuildSettings.PodCount-count; i++ {
 			c.CreatePod()
 
 		}
+		time.Sleep(30 * time.Second)
 
-		time.Sleep(60 * time.Second)
 	}
 
 }
 
+func (c *Controller) handleSchedAdd(newObj interface{}) {
+	pod, ok := newObj.(*v1.Pod)
+	if !ok {
+		c.Logger.Info("failed to cast object")
+		return
+	}
+
+	if c.okToRecreate(pod) {
+		c.Logger.Info("We have a pod")
+		c.Logger.Sugar().Warnf("New pod is : %v", newObj)
+		c.CreatePod()
+
+	}
+}
 func (c *Controller) recreatePod(oldObj, newObj interface{}) {
-	pod := newObj.(*v1.Pod)
+	c.Logger.Info("update event We have a pod")
+	c.Logger.Sugar().Infof("New old is : %v", oldObj)
+
+	c.Logger.Sugar().Infof("New pod is : %v", newObj)
+	/*pod := newObj.(*v1.Pod)
 	if c.okToRecreate(pod) {
 		c.Logger.Info("We have a pod")
 		c.Logger.Sugar().Warnf("New pod is : %v", newObj)
@@ -127,17 +158,8 @@ func (c *Controller) recreatePod(oldObj, newObj interface{}) {
 			go c.deletePod(pod)
 			c.CreatePod()
 		}
-		// And delete the previous one
-		/*
-			err := c.deletePod(pod)
-			if err != nil {
-				c.Logger.Error("Failed to delete pod: ", zap.Error(err))
-			} else {
-				// We create a new pod. TODO : This needs to be passed to a worker queue
-				c.CreatePod() // TODO : need a better way of doing this
-			}
-		*/
-	}
+
+	}*/
 }
 
 func (c *Controller) okToRecreate(pod *v1.Pod) bool {
@@ -157,6 +179,10 @@ func (c *Controller) isPodUnhealthy(pod *v1.Pod) bool {
 		}
 	}
 	return false
+}
+func (c *Controller) handleSchedDelete(obj interface{}) {
+	c.Logger.Sugar().Warnf("delete pod is : %v", obj)
+
 }
 
 func (c *Controller) deletePod(pod *v1.Pod) error {
